6b2935a74362d79a54869c56cac8fa31b33f3325
kosipov <catalonec1994@gmail.com>
2022-11-23 12:57:24 +0300

    Update ctop.sh

--- ctop.sh
+++ ctop.sh
@@ -1,9 +1,4 @@
 #/bin/sh
 
-sudo apt-get install ca-certificates curl gnupg lsb-release
-curl -fsSL https://azlux.fr/repo.gpg.key | sudo gpg --dearmor -o /usr/share/keyrings/azlux-archive-keyring.gpg
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/azlux-archive-keyring.gpg] http://packages.azlux.fr/debian \
-  $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/azlux.list >/dev/null
-sudo apt-get update
-sudo apt-get install docker-ctop
+sudo wget https://github.com/bcicen/ctop/releases/download/v0.7.1/ctop-0.7.1-linux-amd64 -O /usr/local/bin/ctop
+sudo chmod +x /usr/local/bin/ctop
